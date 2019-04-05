f304acdf33e3b5e8c8386f26f343c2b4b3495b2f
starovoitov anton <starovoitov.anton@gmail.com>
2019-04-05 09:43:59 +0500

    added async blinking star

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 import time
 from curses import wrapper
 import curses
+import asyncio
+
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
 def draw(canvas):
     row, column = (5, 20)
     curses.curs_set(0)
     canvas.border()
-    star = "*"
+    # star = "*"
+    coroutine = blink(canvas, row, column)
     while True:
-        canvas.addstr(row, column, star, curses.A_DIM)
+        coroutine.send(None)
         canvas.refresh()
-        time.sleep(2)
-        canvas.addstr(row, column, star)
-        canvas.refresh()
-        time.sleep(0.3)
-        canvas.addstr(row, column, star, curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
-        canvas.addstr(row, column, star)
-        canvas.refresh()
+        time.sleep(1)
+        # canvas.addstr(row, column, star, curses.A_DIM)
+        # canvas.refresh()
+        # time.sleep(2)
+        # canvas.addstr(row, column, star)
+        # canvas.refresh()
+        # time.sleep(0.3)
+        # canvas.addstr(row, column, star, curses.A_BOLD)
+        # canvas.refresh()
+        # time.sleep(0.5)
+        # canvas.addstr(row, column, star)
+        # canvas.refresh()
 
 
 if __name__ == '__main__':
